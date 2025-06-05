8fedaf2bea1ddf281e7a1a61d8fa3fe002243cd8
praveen1024 <mandalapupraveen2000@gmail.com>
2025-06-05 18:31:49 -0400

    Fix the issue with Chakra Menus

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, HStack, Show } from '@chakra-ui/react'
+import { Box, Flex, Grid, GridItem, HStack, Show } from '@chakra-ui/react'
 import { P } from 'framer-motion/dist/types.d-CtuPurYT'
 import { useState } from 'react'
 import GameGrid from './components/GameGrid'
@@ -40,10 +40,12 @@ function App() {
     </Show>
     <GridItem area='main' >
       {/* <PlatformSelector selectedPlatform={selectedPlatform} onSelectPlatform={(platform) => setSelectedPlatform(platform)} /> */}
-      <HStack spacing={5} paddingLeft={2} marginBottom={4} >
-        <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform })} />
+      <Flex paddingLeft={2} marginBottom={4} >
+        <Box marginRight={5}>
+          <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform })} />
+        </Box>
         <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({ ...gameQuery, sortOrder })} />
-      </HStack>
+      </Flex>
       <GameGrid gameQuery={gameQuery} />
       {/* <GameGrid selectedPlatform={gameQuery.platform} selectedGenre={gameQuery.genre} /> */}
       {/* <GameGrid selectedPlatform={selectedPlatform} selectedGenre={selectedGenre} /> */}
